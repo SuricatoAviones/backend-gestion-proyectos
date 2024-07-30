d994352ebf2ede80a4f93da56873096b25504008
Luis <luis.angel151199@gmail.com>
2024-07-30 00:38:35 -0400

    seguimiento added

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,7 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 import { Project } from "src/projects/entities/project.entity";
+import { Tracking } from "src/trackings/entities/tracking.entity";
 import { User } from "src/users/entities/user.entity";
 
 export class CreateTaskDto {
@@ -19,4 +20,9 @@ export class CreateTaskDto {
   @IsNotEmpty()
   @Type( () => Project)
   i013f_i003t_entrada: Project;
+
+  @ApiProperty()
+  @IsOptional()
+  @Type( () => Tracking)
+  i013f_i014t_seguimiento: Tracking
 }
