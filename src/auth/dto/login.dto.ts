8bfdf7b9a15acb0924a956863def75dcb17c7c72
Luis <luis.angel151199@gmail.com>
2024-08-04 16:27:15 -0400

    xd

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -4,9 +4,6 @@ import { IsEmail, IsString, MinLength } from "class-validator";
 
 export class LoginDto{
     @ApiProperty()
-    @IsEmail()
-    in_correo: string;
-    @ApiProperty()
     @IsString()
     @MinLength(4)
     password: string;
